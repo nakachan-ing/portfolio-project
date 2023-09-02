e861a6655d105cb176a8d93649e422d26230ef4f
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-09-02 23:40:36 +0900

    Mapper.javaの追加(プロジェクト付加情報)

---
+++ src/main/java/com/example/demo/mapper/CommunicationTypeMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.entity.CommunicationType;
+
+@Mapper
+public interface CommunicationTypeMapper {
+
+	List<CommunicationType> findAll();
+}
